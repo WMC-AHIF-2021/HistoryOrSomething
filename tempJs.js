38967f15a11fa0436f417355b298d996532b2e6b
Christian-Ekhator <christian.ekhator06@gmail.com>
2022-01-30 11:10:04 +0100

    PreLoader Added to Index

--- tempJs.js
+++ tempJs.js
@@ -1,31 +1,35 @@
-$('.carousel').carousel({
-    interval: 5000,
-    transition: 2000
-})
+$(".carousel").carousel({
+  interval: 5000,
+  transition: 2000,
+});
 
-let things =
-    [
-        "The Mayan people worshipped turkeys like Gods.",
-        "Napoleon was once attacked by a horde of bunnies.",
-        "Cleopatra was not Egyptian, but Greek.",
-        "Pope Gregory IV declared war on cats.",
-        "In the 1830s, ketchup was sold as medicine.",
-        "Walt Disney didn't draw Mickey Mouse.",
-        "Johnny Appleseed is real.",
-        "Former U.S. President Bill Clinton lost Nuclear Launch Codes.",
-        "The dutch actually ate their prime minister.",
-        "Australia actually fought a war against its national bird ...and lost!",
-        "In Liechtenstein's last military engagement (1886)," +
-        "they sent in 80 soldiers. " +
-        "81 came back, including a new italian friend."
-    ];
-let ran = Math.floor(Math.random()*things.length);
+let things = [
+  "The Mayan people worshipped turkeys like Gods.",
+  "Napoleon was once attacked by a horde of bunnies.",
+  "Cleopatra was not Egyptian, but Greek.",
+  "Pope Gregory IV declared war on cats.",
+  "In the 1830s, ketchup was sold as medicine.",
+  "Walt Disney didn't draw Mickey Mouse.",
+  "Johnny Appleseed is real.",
+  "Former U.S. President Bill Clinton lost Nuclear Launch Codes.",
+  "The dutch actually ate their prime minister.",
+  "Australia actually fought a war against its national bird ...and lost!",
+  "In Liechtenstein's last military engagement (1886)," +
+    "they sent in 80 soldiers. " +
+    "81 came back, including a new italian friend.",
+];
+let ran = Math.floor(Math.random() * things.length);
 
 function openForm() {
-    ran = Math.floor(Math.random()*things.length);
-    document.getElementById("myForm").style.display = "block";
-    document.getElementById("facts").innerHTML = things[ran];
+  ran = Math.floor(Math.random() * things.length);
+  document.getElementById("myForm").style.display = "block";
+  document.getElementById("facts").innerHTML = things[ran];
 }
 function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+  document.getElementById("myForm").style.display = "none";
 }
+
+window.addEventListener("load", () => {
+  const preload = document.querySelector(".preload");
+  preload.classList.add("preload-finish");
+});
