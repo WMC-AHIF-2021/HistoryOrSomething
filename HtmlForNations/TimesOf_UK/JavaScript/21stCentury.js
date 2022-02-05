43b624d6015a786065f806370991ff32ef875e25
Christian-Ekhator <christian.ekhator06@gmail.com>
2022-02-05 23:56:13 +0100

    Changes

--- HtmlForNations/TimesOf_UK/JavaScript/21stCentury.js
+++ HtmlForNations/TimesOf_UK/JavaScript/21stCentury.js
@@ -1,28 +1,3 @@
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
-
-  burger.addEventListener("click", () => {
-    //Toggle Nav
-    nav.classList.toggle("nav-active");
-
-    //Animate Links
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 10 + 1
-        }s`;
-      }
-    });
-
-    // Burger Animation
-    burger.classList.toggle("toggle");
-  });
-};
-
 let things = [
   "The Mayan people worshipped turkeys like Gods.",
   "Napoleon was once attacked by a horde of bunnies.",
@@ -51,22 +26,7 @@ function openForm() {
   document.getElementById("myForm").style.display = "block";
   document.getElementById("facts").innerHTML = things[ran];
 }
+
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
-// Dark Mode
-
-let icon = document.getElementById("icon");
-
-icon.onclick = function () {
-  document.body.classList.toggle("light-mode");
-
-  if (document.body.classList.contains("light-mode")) {
-    icon.src = "../Icons/moon.png";
-    //document.getElementById("table").className = "table table-dark table-hover";
-  } else {
-    icon.src = "../Icons/sun.png";
-  }
-};
-
-navSlide();
