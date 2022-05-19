8317613ed5567d6e5a47c3ab17099a9424f0d68f
Yannik <ylisa0102@gmail.com>
2022-05-19 13:05:32 +0200

    UpdateCountries

---
+++ src/scripts/CountrySwitch.ts
@@ -0,0 +1,58 @@
+let europeNames: string[][] = [
+    ["Germany", "../Resources/flags/germany.png", "../Nations/TimesOf_Germany/src/german-present.html"],
+    ["Austria", "../Resources/flags/austria.png", ""],
+    ["France", "../Resources/flags/france.png", "../Nations/TimesOf_France/src/FrancePresent.html"],
+    ["Italy", "../Resources/flags/italy.png", ""],
+    ["Spain", "../Resources/flags/spain.png", "../Nations/TimesOf_Spain/src/SpainPresent.html"],
+    ["The UK", "../Resources/flags/united-kingdom.png", "../Nations/TimesOf_UK/src/UK2022.html"],
+    ["Greece", "../Resources/flags/greece.png", ""]
+]
+
+let asiaNames: string[][] = [
+    ["Japan", "../Resources/flags/japan.png", "../Nations/TimesOf_Japan/src/Japan2022.html"],
+    ["China", "../Resources/flags/china.png", ""]
+]
+
+function switchContinent(){
+    let sectionHeader = document.getElementById('sectionHeaderTS');
+    let continent = sectionHeader.innerText;
+
+    switch(continent){
+        case "Europe":
+            document.getElementById('sectionHeaderBackground').style.backgroundImage =
+                "linear-gradient(var(--background-image-curtain), var(--background-image-curtain)), url(\"../Resources/neededImg/AsiaImg.jpg\")";
+            document.getElementById('sectionHeaderTS').innerText = "Asia";
+            changeContent(asiaNames);
+            break;
+        default:
+            document.getElementById('sectionHeaderBackground').style.backgroundImage =
+                "linear-gradient(var(--background-image-curtain), var(--background-image-curtain)), url(\"../Resources/neededImg/EuropeImg.jpg\")";
+            document.getElementById('sectionHeaderTS').innerText = "Europe";
+            changeContent(europeNames);
+            break;
+    }
+}
+
+function changeContent(arr: string[][]){
+    let boxes: HTMLCollectionOf<Element> = document.getElementsByClassName('countryBox');
+    for(let x: number = 0; x < arr.length || x < boxes.length; x++){
+        let box = boxes.item(x) as HTMLElement;
+        if(x >= arr.length){
+            box.style.display = "none";
+            box.innerHTML = "";
+        }
+        else{
+            box.style.display = "flex";
+            box.addEventListener('click', () => {
+                location.href = arr[x][2];
+            })
+            box.innerHTML =
+                "<img src='" + arr[x][1] + "' alt='" + arr[x][0] + " Flag' />\n" +
+                "<span>" + arr[x][0] + "</span>";
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    switchContinent();
+} )
