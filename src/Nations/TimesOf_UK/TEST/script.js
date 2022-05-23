4e4c1904ef815ebc3886908d386e202e04510175
Christian-Ekhator <christian.ekhator06@gmail.com>
2022-05-23 10:11:00 +0200

    nav-links disabled

--- src/Nations/TimesOf_UK/TEST/script.js
+++ src/Nations/TimesOf_UK/TEST/script.js
@@ -6,3 +6,20 @@ function goToQuiz(value){
     localStorage.setItem('quiz', value);
     window.location.href = "http://localhost:63342/HistoryOrSomething/src/Quiz/index.html";
 }
+
+const selected = document.querySelector(".selected");
+
+const optionsContainer = document.querySelector(".options-container");
+
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+    optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach(o => {
+    o.addEventListener("click", () => {
+        selected.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainer.classList.remove("active");
+    });
+});
