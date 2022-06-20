eb7bd43785e1f27a1e7c150c1fde5c89eca5d61d
Christian-Ekhator <christian.ekhator06@gmail.com>
2022-06-20 08:46:56 +0200

    changes

--- src/Quiz/sript.js
+++ src/Quiz/sript.js
@@ -1,4 +1,4 @@
 function goToQuiz(value){
     localStorage.setItem('quiz', value);
-    window.location.href = "http://localhost:63342/HistoryOrSomethingMain/src/Quiz/Temp/index.html?_ijt=d0hod6blklucefevklnitokn0";
+    window.location.href = "http://HistoryOrSomethingMain/src/Quiz/Temp/index.html";
 }
